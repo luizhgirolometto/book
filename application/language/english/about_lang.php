0711377da845f22d6581a950e06a26b193216ea5
Marlon <marlon@xpert.com.br>
2018-05-15 10:32:38 -0300

    Traduções

--- application/language/english/about_lang.php
+++ application/language/english/about_lang.php
@@ -1,10 +1,10 @@
 <?php $lang['page_name'] = "about_lang";
 $lang['language'] = "english";
-$lang['about_A1'] = "About Us";
-$lang['about_A4'] = "Easy to Find";
-$lang['about_A5'] = "Instant Booking";
-$lang['about_A6'] = "24 Hours Service";
-$lang['about_A7'] = "Read more";
-$lang['about_A2'] = "Founded in 2007 with the mission & oacute; n to improve access to healthcare, Book My Doc is a free service that allows patients to find the best Doctors near by You.  ";
+$lang['about_A1'] = "sobre nós";
+$lang['about_A4'] = "Encontre facilmente";
+$lang['about_A5'] = "Agendamento instantâneo";
+$lang['about_A6'] = "Serviço 24 horas";
+$lang['about_A7'] = "Veja mais";
+$lang['about_A2'] = "Fundado em 2018, o MarqueHora tem a missão de melhorar o acesso aos cuidados da saúde e da beleza da mulher e do homem. O MarqueHora é um serviço gratuito que permite clientes encontrarem os melhores profissionais.";
 $lang['about_A3'] = "see their availability in real time, and instantly you book an appointment to trav & eacute; s book My Doc .com shareware or freeware book My Doc for iPhone or Android. By revealing the hidden supply quotations, book My Doc helping senior RIVER patients to have access to the care & oacute; n in s & oacute; the 24 - to 72 hours. Product m & aacute; recent s of CO-& iacute; a, book My Doc Check-In, allows patients to fill their tr & aacute; limits on L & iacute; nea before his appointment, and a version & oacute; n espa TODDLER language; ol called my book Doc in Spain TODDLER ol also & eacute; n est & aacute; available.  ";
  ?>
--- application/language/english/clinicprofile_lang.php
+++ application/language/english/clinicprofile_lang.php
@@ -1,15 +1,15 @@
 <?php $lang['page_name'] = "clinicprofile_lang";
 $lang['language'] = "english";
-$lang['clinicprofile_slide_A1'] = "Gallery";
-$lang['clinicprofile_slide_A2'] = "About";
-$lang['clinicprofile_slide_A3'] = "Specialities";
-$lang['clinicprofile_slide_A4'] = "Amenities";
-$lang['clinicprofile_slide_A7'] = "Select a Speciality Doctor";
-$lang['clinicprofile_slide_A8'] = "View Profile";
-$lang['clinicprofile_slide_A5'] = "Memberships";
-$lang['clinicprofile_slide_A6'] = "Awards";
-$lang['clinicprofile_slide_A9'] = "Book Online";
-$lang['clinicprofile_slide_A10'] = "Sorry, No records found. Please try with different keywords";
-$lang['clinicprofile_slide_A11'] = "Appointment Details";
-$lang['clinicprofile_slide_A12'] = "Close";
+$lang['clinicprofile_slide_A1'] = "Galeria";
+$lang['clinicprofile_slide_A2'] = "sobre";
+$lang['clinicprofile_slide_A3'] = "Especialidades";
+$lang['clinicprofile_slide_A4'] = "Facilidades";
+$lang['clinicprofile_slide_A7'] = "Selecione as especialidades";
+$lang['clinicprofile_slide_A8'] = "Ver perfil";
+$lang['clinicprofile_slide_A5'] = "Associações";
+$lang['clinicprofile_slide_A6'] = "Prêmios";
+$lang['clinicprofile_slide_A9'] = "Agendamentos Online";
+$lang['clinicprofile_slide_A10'] = "Desculpe, Nenhum registro encontrado. Por favor, tente com filtros diferentes";
+$lang['clinicprofile_slide_A11'] = "Detalhes do agendamento";
+$lang['clinicprofile_slide_A12'] = "Fechar";
  ?>
