a77ff0a1878c1ae088e94617460e995e87510cd3
luizhgirolometto@hotmail.com <hewlet123>
2018-05-15 20:39:16 -0300

    ARQUIVOS CORRIGIDOS

--- application/language/english/clinicfilter_lang.php
+++ application/language/english/clinicfilter_lang.php
@@ -1,15 +1,15 @@
 <?php $lang['page_name'] = "clinicfilter_lang";
 $lang['language'] = "english";
-$lang['clinicfilter_slide_A1'] = "City";
-$lang['clinicfilter_slide_A2'] = "select City";
-$lang['clinicfilter_slide_A3'] = "Specialty";
-$lang['clinicfilter_slide_A4'] = "Select specialty";
-$lang['clinicfilter_slide_A7'] = "Insurance";
-$lang['clinicfilter_slide_A8'] = "Select Insurance";
-$lang['clinicfilter_slide_A5'] = "Reason";
-$lang['clinicfilter_slide_A6'] = "Select reason";
-$lang['clinicfilter_slide_A9'] = "clinics near by You";
-$lang['clinicfilter_slide_A10'] = "Select a Clinic";
-$lang['clinicfilter_slide_A11'] = "View";
-$lang['clinicfilter_slide_A12'] = "Sorry, No records found. Please try with different keywords";
+$lang['clinicfilter_slide_A1'] = "Cidade";
+$lang['clinicfilter_slide_A2'] = "Selecione a cidade";
+$lang['clinicfilter_slide_A3'] = "Especialidade";
+$lang['clinicfilter_slide_A4'] = "Selecione a especialidade";
+$lang['clinicfilter_slide_A7'] = "Plano de Saúde";
+$lang['clinicfilter_slide_A8'] = "Selecione o plano de saúde";
+$lang['clinicfilter_slide_A5'] = "Razão";
+$lang['clinicfilter_slide_A6'] = "Selecione a razão";
+$lang['clinicfilter_slide_A9'] = "clínicas perto de você";
+$lang['clinicfilter_slide_A10'] = "Selecione a clínica";
+$lang['clinicfilter_slide_A11'] = "Ver";
+$lang['clinicfilter_slide_A12'] = "Desculpe, Nenhum registro encontrado. Por favor, tente com filtros diferentes";
  ?>
