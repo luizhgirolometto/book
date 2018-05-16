ddc1c86316ca29dda1d272847b1d69c5de191dec
Marlon Cesar Pilonetto <marlon.pilonetto@gmail.com>
2018-05-16 08:41:25 -0300

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -1,28 +1,47 @@
 <?php defined("BASEPATH") OR exit("No direct script access allowed");$active_group = "default";
-          $query_builder = TRUE;
-$db["default"] = array("dsn"  => "",
-//"hostname" => "br250.hostgator.com.br", 
-//"username" => "giromeca_bookuse" ,
-//"password" => "Fujilink1018@",
-//"database" => "giromeca_book01",
-"hostname" => "localhost", 
+          $query_builder = TRUE;
 
-"username" => "root" ,
+$db["default"] = array("dsn"  => "",
 
-"password" => "",
+"hostname" => "br250.hostgator.com.br", 
 
-"database" => "marquehora",
+"username" => "giromeca_bookuse" ,
 
-"dbdriver" => "mysqli",
-"pconnect" => FALSE,
-"db_debug" => (ENVIRONMENT !== "production"),
-"cache_on" => FALSE,
-"cachedir" => "",
-"char_set" => "utf8",
-"dbcollat" => "utf8_general_ci",
-"swap_pre" => "",
-"encrypt" => FALSE,
-"compress" => FALSE,
-"stricton" => FALSE,
-"failover" => array(),
-"save_queries" => TRUE);
+"password" => "Fujilink1018@",
+
+"database" => "giromeca_book01",
+
+//"hostname" => "localhost", 
+
+//"username" => "root" ,
+
+//"password" => "",
+
+//"database" => "marquehora",
+
+"dbdriver" => "mysqli",
+
+"pconnect" => FALSE,
+
+"db_debug" => (ENVIRONMENT !== "production"),
+
+"cache_on" => FALSE,
+
+"cachedir" => "",
+
+"char_set" => "utf8",
+
+"dbcollat" => "utf8_general_ci",
+
+"swap_pre" => "",
+
+"encrypt" => FALSE,
+
+"compress" => FALSE,
+
+"stricton" => FALSE,
+
+"failover" => array(),
+
+"save_queries" => TRUE);
+
