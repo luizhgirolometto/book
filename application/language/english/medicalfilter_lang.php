a997a757cbd9c797ed0c5c073d91370540a3e64c
Luiz Henrique Girolometto <luizhgirolometto@hotmail.com>
2018-05-18 15:35:18 -0300

    tradução

--- application/language/english/medicalfilter_lang.php
+++ application/language/english/medicalfilter_lang.php
@@ -1,15 +1,15 @@
 <?php $lang['page_name'] = "medicalfilter_lang";
 $lang['language'] = "english";
-$lang['medicalfilter_slide_A1'] = "City";
-$lang['medicalfilter_slide_A2'] = "select City";
-$lang['medicalfilter_slide_A3'] = "Specialty";
-$lang['medicalfilter_slide_A4'] = "Select specialty";
-$lang['medicalfilter_slide_A7'] = "Insurance";
-$lang['medicalfilter_slide_A8'] = "Select Insurance";
-$lang['medicalfilter_slide_A5'] = "Reason";
-$lang['medicalfilter_slide_A6'] = "Select reason";
-$lang['medicalfilter_slide_A12'] = "Medicals near by You";
-$lang['medicalfilter_slide_A13'] = "Select a Medical";
-$lang['medicalfilter_slide_A14'] = "View";
-$lang['medicalfilter_slide_A15'] = "Sorry, No records found. Please try with different keywords.";
+$lang['medicalfilter_slide_A1'] = "Cidade";
+$lang['medicalfilter_slide_A2'] = "selecione Cidade";
+$lang['medicalfilter_slide_A3'] = "Especialidade";
+$lang['medicalfilter_slide_A4'] = "Selecione Especialidade";
+$lang['medicalfilter_slide_A7'] = "Convênio";
+$lang['medicalfilter_slide_A8'] = "Selecione Convênio";
+$lang['medicalfilter_slide_A5'] = "Motivo";
+$lang['medicalfilter_slide_A6'] = "Selecione Motivo";
+$lang['medicalfilter_slide_A12'] = "Médicos perto de você";
+$lang['medicalfilter_slide_A13'] = "Selecione Médicos";
+$lang['medicalfilter_slide_A14'] = "Ver";
+$lang['medicalfilter_slide_A15'] = "Desculpe, Nenhum registro encontrado. Por favor, tente com palavras-chave diferentes.";
  ?>
