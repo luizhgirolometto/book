980448de8a0eefea6e2827a925ec5266815b4f3e
Marlon <marlon@xpert.com.br>
2018-08-01 17:25:24 -0300

    Perfil

--- application/views/doctor_profile.php
+++ application/views/doctor_profile.php
@@ -8,113 +8,99 @@
         require 'admin/includes/'.$textFile.$extension;
 		echo $map['js']; ?>
 <!--viewprofile-->
-<div class="container-fluid">
-    <div class="col-lg-12 col-pad">
-        <div class="view-head">
-            <div class="col-lg-1"></div>
-            <div class="col-lg-5" style="padding: 0;">
-                <div class="left-view-main">
-					<div class="row">
-						<div class="col-lg-3">
-							<div class="img-pat-view">
-								<?php if($view_doctor->display_image != ""){ ?>
-								<img src= "<?php echo base_url(); ?>admin/<?php echo $view_doctor->display_image;?>" alt=""/>
-								<?php }else{ ?>
-								<img src="<?php echo base_url(); ?>assets/images/home/man.png" alt="" />
-								<?php  }?>
-							</div>
-						</div>
-						<div class="col-lg-9">
-							<div class="pat-view-text">
-								<h3>Dr. <?php echo $view_doctor->doctor_firstname; ?> <?php echo $view_doctor->doctor_lastname;?></h3>
-								<h4><?php if($view_doctor->degree_name != ""){ echo $view_doctor->degree_name; }else{ echo "No Details Found"; } ?> , <?php if($view_doctor->specialty_name != ""){  echo $view_doctor->specialty_name; }else{ echo "No Details Found"; }?></h4>
-								<div class="gc-ratting" data-rate="<?php echo $view_doctor->avg_rating; ?>" ></div>
-							</div>
-						</div>
-					</div>
-                    <div class="row">
-                        <div class="col-lg-3 col-xs-4 col-sm-3">
-							<div class="slider-qualification">
-                                <div class="col-lg-12">
-                                    <div id="myCarousel" class="carousel slide vertical">
-										<div class="carousel-inner sl-slider">
-                                            <div class="active item">
-												<?php if(! empty ($view_galpic[0] )){ ?>
-												<img src= "<?php echo base_url(); ?>admin/<?php echo $view_galpic[0]->image;?>" />
-												<?php }else{ ?>
-												<img src="<?php echo base_url(); ?>assets/images/patient-login/sl-1.png">
-												<?php } ?>
-												<?php if(! empty ($view_galpic[1] )){ ?>
-												<img src= "<?php echo base_url(); ?>admin/<?php echo $view_galpic[1]->image;?>" />
-												<?php }else{ ?>
-                                                <img src="<?php echo base_url(); ?>assets/images/patient-login/sl-1.png">
-												<?php } ?>
-												<?php if(! empty ($view_galpic[2] )){ ?>
-												<img src= "<?php echo base_url(); ?>admin/<?php echo $view_galpic[2]->image;?>" />
-												<?php }else{ ?>
-                                                <img src="<?php echo base_url(); ?>assets/images/patient-login/sl-1.png">
-												<?php } ?>
-                                            </div>
-                                            <div class="item">
-												<?php if(! empty ($view_galpic[3] )){ ?>
-												<img src= "<?php echo base_url(); ?>admin/<?php echo $view_galpic[3]->image;?>" />
-												<?php }else{ ?>
-												<img src="<?php echo base_url(); ?>assets/images/patient-login/sl-1.png">
-												<?php } ?>
-												<?php if(! empty ($view_galpic[4] )){ ?>
-												<img src= "<?php echo base_url(); ?>admin/<?php echo $view_galpic[4]->image;?>" />
-												<?php }else{ ?>
-												<img src="<?php echo base_url(); ?>assets/images/patient-login/sl-1.png">
-												<?php } ?>
-												<?php if(! empty ($view_galpic[5] )){ ?>
-												<img src= "<?php echo base_url(); ?>admin/<?php echo $view_galpic[5]->image;?>" />
-												<?php }else{ ?>
-												<img src="<?php echo base_url(); ?>assets/images/patient-login/sl-1.png">
-												<?php } ?>
-											</div>
-											<div class="item">
-												<?php if(! empty ($view_galpic[6] )){ ?>
-												<img src= "<?php echo base_url(); ?>admin/<?php echo $view_galpic[6]->image;?>" />
-												<?php }else{ ?>
-												<img src="<?php echo base_url(); ?>assets/images/patient-login/sl-1.png">
-												<?php } ?>
-												<?php if(! empty ($view_galpic[7] )){ ?>
-												<img src= "<?php echo base_url(); ?>admin/<?php echo $view_galpic[7]->image;?>" />
-												<?php }else{ ?>
-												<img src="<?php echo base_url(); ?>assets/images/patient-login/sl-1.png">
-												<?php } ?>
-												<?php if(! empty ($view_galpic[8] )){ ?>
-												<img src= "<?php echo base_url(); ?>admin/<?php echo $view_galpic[8]->image;?>" />
-												<?php }else{ ?>
-												<img src="<?php echo base_url(); ?>assets/images/patient-login/sl-1.png">
-												<?php } ?>
-											</div>
-										</div>
-										<!-- Carousel nav -->
-                                        <a class="carousel-control " href="#myCarousel" data-slide="prev"><img class="pre-pos" src="<?php echo base_url(); ?>assets/images/patient-login/pre.png" /> </a>
-                                        <a class="carousel-control " href="#myCarousel" data-slide="next"><img class="next-pos" src="<?php echo base_url(); ?>assets/images/patient-login/next.png" /></a>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-9">
-							<div class="quali-main">
-								<h4><?php if($this->lang->line('doctorprofile_slide_A1')){ ?><?php echo $this->lang->line('doctorprofile_slide_A1'); }else{ ?>Qualification & Booking Appointment<?php } ?></h4>
-								<?php $arry_select = explode(",", $view_doctor->specialty); ?>
-								<h5><?php if($view_doctor->degree_name != ""){ echo $view_doctor->degree_name; }else { echo "No Details Found"; }?> , <?php  foreach($specialtydetails as $detailsspecialty){ if (in_array($detailsspecialty->id, $arry_select)) {   echo $detailsspecialty->specialty_name;  ?>, <?php  }}  ?> </h5>								
-								<p><?php if(!empty($view_doctor->about_doctor )){ echo $view_doctor->about_doctor; }else { echo "No Details Found"; }?> </p>
-							</div>						
-						</div>						
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-6" id="canvas1">
-				<?php echo $map['html']; ?>
-			</div>           
-		</div>
-	</div>
-</div>
+
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Portfolio Item Heading -->
+      <h1 class="my-4"><span>Dr. <?php echo $view_doctor->doctor_firstname; ?> <?php echo $view_doctor->doctor_lastname;?></span>
+          
+      </h1>
+      
+        <div class="gc-ratting" data-rate="<?php echo $review->rating; ?>" ></div> 
+
+      <!-- Portfolio Item Row -->
+      <div class="row">
+
+        <div class="col-md-8"> 
+
+                <?php if($view_doctor->display_image != ""){ ?>
+                    <img class="img-fluid" src="<?php echo base_url(); ?>admin/<?php echo $view_doctor->display_image;?>" alt="">
+				<?php }else{ ?>
+				    <img class="img-fluid" src="<?php echo base_url(); ?>assets/images/home/man.png" alt="">
+				<?php } ?>             
+          
+        </div>
+
+        <div class="col-md-4">
+          <h3 class="my-3">Sobre</h3>
+          <p><?php if(!empty($view_doctor->about_doctor )){ echo $view_doctor->about_doctor; }else { echo "Sem mais detalhes sobre este profissional."; }?> </p>
+          <h3 class="my-3">Especialidades</h3>
+            <?php $arry_select = explode(",", $view_doctor->specialty); ?>
+          <ul>
+            <li><?php if($view_doctor->degree_name != ""){ echo $view_doctor->degree_name; }else { echo "No Details Found"; }?> , <?php  foreach($specialtydetails as $detailsspecialty){ if (in_array($detailsspecialty->id, $arry_select)) {   echo $detailsspecialty->specialty_name;  ?>, <?php  }}  ?> </li>								              
+          </ul>
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+      <!-- Related Projects Row -->
+      <h3 class="my-4">Galeria de imagens</h3>
+
+      <div class="row">
+
+        <div class="col-md-3 col-sm-6 mb-4">
+          <a href="#">
+                <?php if(! empty ($view_galpic[1] )){ ?>
+                    <img class="img-fluid" src="<?php echo base_url(); ?>admin/<?php echo $view_galpic[1]->image;?>" alt="">
+				<?php }else{ ?>
+				    <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+				<?php } ?> 
+          </a>
+        </div>
+
+        <div class="col-md-3 col-sm-6 mb-4">
+          <a href="#">
+                <?php if(! empty ($view_galpic[2] )){ ?>
+                    <img class="img-fluid" src="<?php echo base_url(); ?>admin/<?php echo $view_galpic[2]->image;?>" alt="">
+				<?php }else{ ?>
+				    <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+				<?php } ?> 
+          </a>
+        </div>
+
+        <div class="col-md-3 col-sm-6 mb-4">
+          <a href="#">
+                <?php if(! empty ($view_galpic[3] )){ ?>
+                    <img class="img-fluid" src="<?php echo base_url(); ?>admin/<?php echo $view_galpic[3]->image;?>" alt="">
+				<?php }else{ ?>
+				    <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+				<?php } ?> 
+          </a>
+        </div>
+
+        <div class="col-md-3 col-sm-6 mb-4">
+          <a href="#">
+                <?php if(! empty ($view_galpic[4] )){ ?>
+                    <img class="img-fluid" src="<?php echo base_url(); ?>admin/<?php echo $view_galpic[4]->image;?>" alt="">
+				<?php }else{ ?>
+				    <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+				<?php } ?> 
+          </a>
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+
+
 <!--viewprofile-->
+<!--
 <div class="container-fluid cont-hospital">
 	<div class="container">
 		<div class="row">
@@ -193,7 +179,7 @@
 			<div class="clearfix"></div><div class="col-lg-1"></div>
 		</div>
 	</div>
-</div>		
+</div>	-->	
 <!----- for modal ---->
 <div class="modal bookingdoc fade" id="myModal-calendar" role="dialog">
 	<div class="modal-dialog">    
